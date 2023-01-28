fb41c1fcff612eaf8389545f53b93a1710de6a0d
leeye51456 <leeye51456@gmail.com>
2023-01-28 23:52:29 +0900

    Feat(core): Allow `class` attribute in markdown

--- src/core/convertMarkdownToHast.ts
+++ src/core/convertMarkdownToHast.ts
@@ -1,12 +1,20 @@
 import { Root } from 'hast'
 import rehypeMinifyWhitespace from 'rehype-minify-whitespace'
 import rehypeRaw from 'rehype-raw'
-import rehypeSanitize from 'rehype-sanitize'
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import remarkBreaks from 'remark-breaks'
 import remarkGfm from 'remark-gfm'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import { unified } from 'unified'
+
+const sanitizationSchema: typeof defaultSchema = {
+  ...defaultSchema,
+  attributes: {
+    ...defaultSchema.attributes,
+    '*': [...(defaultSchema.attributes['*'] ?? []), ['className']],
+  },
+}
 
 async function convertMarkdownToHast(markdown: string): Promise<Root> {
   const mdast = unified()
@@ -19,7 +27,7 @@ async function convertMarkdownToHast(markdown: string): Promise<Root> {
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeRaw)
     .use(rehypeMinifyWhitespace)
-    .use(rehypeSanitize)
+    .use(rehypeSanitize, sanitizationSchema)
     .run(mdast)
 }
 
