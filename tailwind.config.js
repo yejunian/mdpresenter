32fb3cdf2024628faa116342ff886249cd1ddf01
leeye51456 <leeye51456@gmail.com>
2023-01-28 23:56:10 +0900

    Config(tailwind): Override default screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+/**
+ * @param {number} columnCount
+ * @returns {string}
+ */
+function getPageListWidth(columnCount) {
+  return `${columnCount * 264 + (columnCount + 1) * 24}px`
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+
   theme: {
     extend: {},
+
+    screens: {
+      sm: getPageListWidth(4),
+      md: getPageListWidth(5),
+      lg: getPageListWidth(6),
+    },
   },
+
   plugins: [],
 }
