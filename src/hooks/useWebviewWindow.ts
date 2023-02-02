1e9ab93926daa04a6102cb637dfeae2931ff21a9
leeye51456 <leeye51456@gmail.com>
2023-02-02 19:54:12 +0900

    Feat(hooks): Implement `useWebviewWindow`

---
+++ src/hooks/useWebviewWindow.ts
@@ -0,0 +1,37 @@
+import { WebviewWindow, WindowOptions } from '@tauri-apps/api/window'
+import { useState } from 'react'
+
+function useWebviewWindow(label: string, options?: WindowOptions) {
+  const [webview, setWebview] = useState<WebviewWindow>(null)
+  const [isProcessing, setProcessing] = useState(false)
+  let isLocallyProcessing = isProcessing
+
+  const create = async () => {
+    if (!webview && !isLocallyProcessing) {
+      isLocallyProcessing = true
+      setProcessing(true)
+
+      const WebviewWindow = (await import('@tauri-apps/api/window'))
+        .WebviewWindow
+
+      const localWebview = new WebviewWindow(label, options)
+
+      localWebview.once(`${label}:load`, () => {
+        localWebview.show()
+      })
+
+      localWebview.once('tauri://destroyed', () => {
+        isLocallyProcessing = true
+        setWebview(null)
+      })
+
+      setWebview(localWebview)
+
+      setProcessing(false)
+    }
+  }
+
+  return { webview, create }
+}
+
+export default useWebviewWindow
