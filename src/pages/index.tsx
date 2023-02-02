ff4a8f00680eddee1ecb6a5ec2e88b82de835bcd
leeye51456 <leeye51456@gmail.com>
2023-02-03 00:06:44 +0900

    Feat(pages): Implement rough UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,104 @@
-function App() {
-  return (
-    <div>
-      <h1>MD Presenter</h1>
+import { ask } from '@tauri-apps/api/dialog'
+import { emit } from '@tauri-apps/api/event'
+import clsx from 'clsx'
+import { useEffect, useState } from 'react'
 
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+import PageList from '../components/PageList'
+import Playback from '../components/Playback'
+import Page from '../core/Page'
+import convertMarkdownToHast from '../core/convertMarkdownToHast'
+import openSingleTextFile from '../core/openSingleTextFile'
+import splitHastRoot from '../core/splitHastRoot'
+import useWebviewWindow from '../hooks/useWebviewWindow'
+
+function App() {
+  const [fileName, setFileName] = useState('')
+  const [fileContents, setFileContents] = useState('')
+
+  const [pages, setPages] = useState<Page[]>([])
+  const [previewPageNumber, setPreviewPageNumber] = useState(14)
+  const [programPageNumber, setProgramPageNumber] = useState(11)
+
+  const { webview: onairWindow, create: createOnairWindow } = useWebviewWindow(
+    'onair',
+    {
+      url: '/onair',
+      title: '송출 - MD Presenter',
+      visible: false,
+      // alwaysOnTop: true,
+      // decorations: false,
+      // fullscreen: true,
+    }
+  )
+
+  useEffect(() => {
+    convertMarkdownToHast(fileContents).then((hastRoot) => {
+      setPages(splitHastRoot(hastRoot))
+    })
+  }, [fileContents])
+
+  const handleOpenClick = async () => {
+    try {
+      const { path, contents } = await openSingleTextFile()
+      setFileName(path)
+      setFileContents(contents)
+    } catch (error) {
+      // No operation
+    }
+  }
+
+  const handleOnairLoad = () => {
+    emit('main:init-onair', pages[programPageNumber - 1])
+  }
+
+  const handleOnairClick = async () => {
+    if (!onairWindow) {
+      createOnairWindow(handleOnairLoad)
+    } else if (await ask('송출 창을 닫을까요?', { type: 'warning' })) {
+      onairWindow.close()
+    }
+  }
+
+  return (
+    <div className="break-words break-keep">
+      <h1 className="mb-4 font-bold">MD Presenter</h1>
+
+      <div className="my-4">
+        <button
+          className="rounded px-4 py-1 bg-blue-600 text-white font-bold"
+          type="button"
+          onClick={handleOpenClick}
+        >
+          Open
+        </button>{' '}
+        <code className="ml-4 text-sm">{fileName || '(not selected)'}</code>
       </div>
+
+      <div className="my-4">
+        <button
+          className={clsx(
+            'border-2 rounded px-4 py-1 font-bold',
+            onairWindow
+              ? 'border-transparent bg-red-700 text-white'
+              : 'border-green-700 text-zinc-300'
+          )}
+          type="button"
+          onClick={handleOnairClick}
+        >
+          ON AIR
+        </button>
+      </div>
+
+      <Playback
+        preview={pages[previewPageNumber - 1] ?? null}
+        program={pages[programPageNumber - 1] ?? null}
+      />
+
+      <PageList
+        pages={pages}
+        previewPageNumber={previewPageNumber}
+        programPageNumber={programPageNumber}
+      />
     </div>
   )
 }
---
+++ src/pages/onair.tsx
@@ -0,0 +1,37 @@
+import { emit, listen, once } from '@tauri-apps/api/event'
+import { useEffect, useState } from 'react'
+
+import Page from '../core/Page'
+import convertHastToReactElements from '../core/convertHastToReactElement'
+import clsx from 'clsx'
+
+function Onair() {
+  const [page, setPage] = useState<Page>(null)
+
+  useEffect(() => {
+    once<Page>('main:init-onair', (event) => {
+      setPage(event.payload)
+    })
+
+    listen<Page>('main:cut', (event) => {
+      setPage(event.payload)
+    })
+
+    emit('onair:load')
+  }, [])
+
+  return (
+    <div
+      className={clsx(
+        'absolute inset-0 overflow-hidden',
+        'flex justify-center items-center',
+        'px-[5vw] py-[5vh] w-full h-full bg-black',
+        'leading-snug text-center font-semibold text-[8vh] text-white'
+      )}
+    >
+      {convertHastToReactElements(page?.contents)}
+    </div>
+  )
+}
+
+export default Onair
