13fe4b6d9367c8a3b663dc077af07cc672aa44f8
leeye51456 <leeye51456@gmail.com>
2023-02-20 23:53:00 +0900

    Feat(getStyle): Split function

--- src/core/getStyle.ts
+++ src/core/getStyle.ts
@@ -2,36 +2,67 @@ import { CSSProperties } from 'react'
 
 import { Config, defaultAppConfig } from './Config'
 
-function addUnitToPadding(source: string): string {
-  const parts = source.split(/\s+/)
+const thumbnailHeight = 144
+const thumbnailRenderFactor = 2
 
-  switch (parts.length) {
-    case 0:
-      return ''
+function addUnitToPadding(
+  source: string,
+  options?: { isForThumbnails?: boolean; aspectRatio?: number }
+): string {
+  const { isForThumbnails, aspectRatio } = options ?? {}
 
-    case 1:
-      return `${parts[0]}vh ${parts[0]}vw`
-
-    case 2:
-      return `${parts[0]}vh ${parts[1]}vw`
-
-    case 3:
-      return `${parts[0]}vh ${parts[1]}vw ${parts[2]}vh`
-
-    default:
-      return `${parts[0]}vh ${parts[1]}vw ${parts[2]}vh ${parts[3]}vw`
+  let parts: number[]
+  try {
+    parts = source.split(/\s+/).map((value) => parseFloat(value))
+  } catch (error) {
+    return ''
   }
+
+  if (parts.length === 1) {
+    if (isForThumbnails) {
+      const paddingY = getAbsoluteSize(parts[0]) * thumbnailRenderFactor
+      const paddingX = paddingY * aspectRatio
+      return `${paddingY}px ${paddingX}px`
+    } else {
+      return `${parts[0]}vh ${parts[0]}vw`
+    }
+  }
+
+  return parts
+    .reduce<string[]>((acc, value, index) => {
+      if (index > 3) {
+        return acc
+      }
+
+      if (isForThumbnails) {
+        const sideFactor = index % 2 === 0 ? 1 : aspectRatio
+        const padding =
+          getAbsoluteSize(value) * sideFactor * thumbnailRenderFactor
+        acc.push(`${padding}px`)
+      } else {
+        acc.push(index % 2 === 0 ? `${value}vh` : `${value}vw`)
+      }
+
+      return acc
+    }, [])
+    .join(' ')
 }
 
-export function getOnairContainerStyle(config: Config): CSSProperties {
-  const style = {
-    fontFamily: config['font-family'] || defaultAppConfig['font-family'],
+function getAbsoluteSize(percentage: number): number {
+  return (percentage / 100) * thumbnailHeight
+}
 
-    fontSize: `${
-      config['font-size'] > 0
-        ? config['font-size']
-        : defaultAppConfig['font-size']
-    }vh`,
+function sanitizeFontSize(fontSize: number): number {
+  return fontSize > 0 ? fontSize : defaultAppConfig['font-size']
+}
+
+function getBaseStyle(config: Config): CSSProperties {
+  return {
+    // NOTE - Undefined properties
+    // - fontSize
+    // - padding
+
+    fontFamily: config['font-family'] || defaultAppConfig['font-family'],
 
     '--font-weight-regular':
       config['font-weight-regular'] || defaultAppConfig['font-weight-regular'],
@@ -58,11 +89,39 @@ export function getOnairContainerStyle(config: Config): CSSProperties {
 
     alignItems:
       config['container-align-y'] || defaultAppConfig['container-align-y'],
+  } as CSSProperties
+}
+
+export function getPageListItemStyle(config: Config): CSSProperties {
+  return {
+    ...getBaseStyle(config),
+
+    fontSize: `${
+      (sanitizeFontSize(config['font-size']) / 100) *
+      thumbnailHeight *
+      thumbnailRenderFactor
+    }px`,
+
+    padding: addUnitToPadding(
+      config['window-padding'] || defaultAppConfig['window-padding'],
+      { isForThumbnails: true, aspectRatio: 16 / 9 }
+    ),
+
+    width: '200%',
+    height: '200%',
+    transform: 'scale(0.5)',
+    transformOrigin: 'top left',
+  } as CSSProperties
+}
+
+export function getOnairContainerStyle(config: Config): CSSProperties {
+  return {
+    ...getBaseStyle(config),
+
+    fontSize: `${sanitizeFontSize(config['font-size'])}vh`,
 
     padding: addUnitToPadding(
       config['window-padding'] || defaultAppConfig['window-padding']
     ),
   } as CSSProperties
-
-  return style
 }
